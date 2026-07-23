8e2f8dbb0c0fd650d1da5feb3e9c054b263f8c41
Ponraja-41 <rajapon41@gmail.com>
2026-07-23 21:15:14 +0530

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,1 +1,14 @@
-print("NSE Institutional Screener")
+import pandas as pd
+
+df = pd.DataFrame({
+    "SYMBOL":["RELIANCE","INFY"],
+    "OPEN":[1500,1800],
+    "HIGH":[1525,1815],
+    "LOW":[1490,1790],
+    "CLOSE":[1518,1810],
+    "VOLUME":[2500000,1800000]
+})
+
+df.to_csv("data/bhavcopy.csv",index=False)
+
+print("Done")
